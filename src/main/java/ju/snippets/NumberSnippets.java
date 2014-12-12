d1518e3c445a3ddd444d448cf55fe3951dc8a9b2
Michael Lane <mlane@gatech.edu>
2014-12-11 23:56:48 -0500

    added functions to convert Lists and Maps of Numbers to those of Doubles

--- src/main/java/ju/snippets/NumberSnippets.java
+++ src/main/java/ju/snippets/NumberSnippets.java
@@ -1,10 +1,29 @@
 package ju.snippets;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author Michael Lane <mlane@gatech.edu>
  */
 public class NumberSnippets {
     
+    public static List<Double> convertToDoubleList(List<? extends Number> list) {
+        List<Double> doubleList = new ArrayList(list.size());
+        for (Number num : list) {
+            doubleList.add(num.doubleValue());
+        }
+        return doubleList;
+    }
     
+    public static <K> Map<K, Double> convertToDoubleMap(Map<K, ? extends Number> map) {
+        Map<K, Double> doubleValues = new HashMap();
+        map.forEach((k, v) -> {
+            doubleValues.put(k, v.doubleValue());
+        });
+        return doubleValues;
+    }
 }
