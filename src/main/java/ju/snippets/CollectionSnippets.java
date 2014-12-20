d34baf131e8167eed2f8bbc843684bcf6206c1ef
Michael Lane <mlane@gatech.edu>
2014-12-20 15:07:07 -0500

    edited and improved javadocs, added the init method, fixed the getMin and getMax (UsingValuesInMap) methods

--- src/main/java/ju/snippets/CollectionSnippets.java
+++ src/main/java/ju/snippets/CollectionSnippets.java
@@ -1,7 +1,11 @@
 package ju.snippets;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
@@ -15,7 +19,13 @@ import ju.tbd.ItemIndexPair;
 public final class CollectionSnippets {
 
     /**
-     * 
+     * Add all the elements of withColl to coll, except the elements that already 
+     * occur in coll. Example:
+     * coll         = [1, 2, 3]
+     * withColl     = [2, 3, 4, 5]
+     * addAllUniquely(coll, withColl)
+     * coll         = [1, 2, 3, 4, 5]
+     * withColl     = [2, 3, 4, 5]
      * @param <T>
      * @param coll
      * @param withColl 
@@ -25,6 +35,24 @@ public final class CollectionSnippets {
             if (!coll.contains(item)) {
                 coll.add(item);
             }
+        }
+    }
+    
+    /**
+     * Add all the elements of withColl to coll, except the elements that already 
+     * occur in coll. Example:
+     * coll         = [1, 2, 3]
+     * withColl     = [2, 3, 4, 5]
+     * addAllUniquely(coll, withColl)
+     * coll         = [1, 2, 3, 4, 5]
+     * withColl     = [2, 3, 4, 5]
+     * @param <T>
+     * @param coll
+     * @param withColl 
+     */
+    public static <T> void addUniquely(Collection<T> coll, T item) {
+        if (!coll.contains(item)) {
+            coll.add(item);
         }
     }
     
@@ -39,12 +67,6 @@ public final class CollectionSnippets {
      * list will be sorted as follows: Georgia, Louisiana, Mississippi, Alabama,
      * Florida.
      * 
-     * For several reasons, before any sorting occurs, the "values" Map is converted
-     * to a new Map that maps keys to Doubles, rather than Numbers. For this reason,
-     * VALUES SHOULD ONLY CONTAIN NUMBERS THAT CAN BE LOSSLESSLY CONVERTED TO DOUBLES.
-     * Such Numbers are: Byte, Short, Integer, Char, Long, Float, or Double. This 
-     * excludes BigInteger and BigDecimal in the standard Java API.
-     * 
      * @param <T>
      * @param list non-null
      * @param values non-null
@@ -56,10 +78,12 @@ public final class CollectionSnippets {
     }
     
     /**
+     * Get the minimum of a List according to values stored in a Map. 
      * @param <K>
      * @param list
      * @param values
-     * @return 
+     * @return The ItemIndexPair representing the minimum Object and its index in 
+     * the List.
      */
     public static <K> ItemIndexPair<K> getMinUsingValuesInMap(
     List<? extends K> list, Map<K, ? extends Comparable> values) {
@@ -75,6 +99,7 @@ public final class CollectionSnippets {
             K item = iter.next();
             Comparable value = values.get(item);
             if (value.compareTo(minValue) < 0) {
+                minItem = item;
                 minIndex = index;
             }
             index++;
@@ -84,10 +109,38 @@ public final class CollectionSnippets {
     }
     
     /**
+     * Get the minimum of a Collection according to values stored in a Map. 
+     * @param <K>
+     * @param coll
+     * @param values
+     * @return 
+     */
+    public static <K> K getMinUsingValuesInMap(
+    Collection<? extends K> coll, Map<K, ? extends Comparable> values) {
+        
+        if (coll.isEmpty()) return null;
+        
+        Iterator<? extends K> iter = coll.iterator();
+        K minItem = iter.next();
+        Comparable minValue = values.get(minItem);
+        while (iter.hasNext()) {
+            K item = iter.next();
+            Comparable value = values.get(item);
+            if (value.compareTo(minValue) < 0) {
+                minItem = item;
+            }
+        }
+        
+        return minItem;
+    }
+    
+    /**
+     * Get the maximum of a List according to values stored in a Map. 
      * @param <K>
      * @param list
      * @param values
-     * @return 
+     * @return The ItemIndexPair representing the maximum Object and its index in 
+     * the List.
      */
     public static <K> ItemIndexPair<K> getMaxUsingValuesInMap(
     List<? extends K> list, Map<K, ? extends Comparable> values) {
@@ -103,11 +156,52 @@ public final class CollectionSnippets {
             K item = iter.next();
             Comparable value = values.get(item);
             if (value.compareTo(maxValue) > 0) {
+                maxItem = item;
                 maxIndex = index;
             }
             index++;
         }
         
         return new ItemIndexPair(maxItem, maxIndex);
+    }
+    
+    /**
+     * Get the maximum of a Collection according to values stored in a Map. 
+     * @param <K>
+     * @param coll
+     * @param values
+     * @return 
+     */
+    public static <K> K getMaxUsingValuesInMap(
+    Collection<? extends K> coll, Map<K, ? extends Comparable> values) {
+        
+        if (coll.isEmpty()) return null;
+        
+        Iterator<? extends K> iter = coll.iterator();
+        K maxItem = iter.next();
+        Comparable maxValue = values.get(maxItem);
+        while (iter.hasNext()) {
+            K item = iter.next();
+            Comparable value = values.get(item);
+            if (value.compareTo(maxValue) < 0) {
+                maxItem = item;
+            }
+        }
+        
+        return maxItem;
+    }
+    
+    /**
+     * Initialize a List with the given items.
+     * @param <T>
+     * @param args
+     * @return 
+     */
+    public static <T> List<T> init(Class<? extends List> cls, T... args) {
+        try {
+            return cls.getDeclaredConstructor(Collection.class).newInstance(args);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Could not instantiate class " + cls.toString());
+        }
     }
 }
